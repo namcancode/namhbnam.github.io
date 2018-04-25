1985bc57e9040a4a2ea34a472647509568c24e29
NamBoss <nguyennamit.94@gmail.com>
2018-04-25 15:22:37 +0700

    B10: BOM

---
+++ Javascript-HTML-Dom/Browser-Object-Model/js/javascript.js
@@ -0,0 +1,19 @@
+vax = ""
+var question1 = {
+    quest: "Câu hỏi 1",
+    answer1: "Trả lời 1",
+    answer2: "Trả lời 2",
+    answer3: "Trả lời 3",
+    answer4: "Trả lời 4",
+}
+var question2 = {
+    quest: "Câu hỏi 2",
+    answer1: "Trả lời 1",
+    answer2: "Trả lời 2",
+    answer3: "Trả lời 3",
+    answer4: "Trả lời 4",
+}
+function getid(bt){
+return document.getElementById(bt)
+}
+
