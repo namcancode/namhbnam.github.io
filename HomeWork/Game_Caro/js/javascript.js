a0a5cf4e25aefa8617df652d3f9a9185c3403b59
NamBoss <nguyennamit.94@gmail.com>
2018-05-14 17:10:36 +0700

    caro: check col and row half a win

--- HomeWork/Game_Caro/js/javascript.js
+++ HomeWork/Game_Caro/js/javascript.js
@@ -1,40 +1,141 @@
 $(function () {
     chessBoard();
     Click();
+    
 })
-var count = 0
 
+
+// function chessBoard() {
+//     for (var i = 1; i <= 374; i++) {
+//         $(".content_wrap").append("<div class='coffer' id = '" + i + "'>"+i+"</div>")
+//     }
+// }
 function chessBoard() {
-    for (var i = 1; i <= 374; i++) {
-        $(".content_wrap").append("<div class='coffer' id = '" + i + "'></div>")
+    for (i = 1; i <= 20; i++) {
+        for (j = 1; j <= 20; j++) {
+            $(".content_wrap").append("<div class='coffer' check = '' j = '" + j + "' i = '" + i + "'></div>")
+        }
+
     }
 }
+var count = 0
+var value = "";
+var result = 0;
+var currentI = "";
+var currentJ = "";
 
 function Click() {
-    
+    var res = 0 ;
     $(".coffer").click(function () {
-        const current = +$(this).attr("id");
-        checkRow()
-        count++;
-              if ($(this).html()=="") {
+        
+        currentI = +$(this).attr("i");
+        currentJ = +$(this).attr("j");
+        console.log("i = " +currentI, "j = " +currentJ);
 
-        if (count % 2 == 1) {
-            $(this).html("X")
-        } else {
-            $(this).html("O")
-        }
-          }
-        function checkRow() {
-            for (var i = current - 4; i < current + 4; i++) {
-             
-                 if($("#"+i).html()=="X" || $("#"+i).html()=="O" ){
-console.log("You Win");
-                 }
-              
-            }
-        }
+        count++;
+        // if ($(this).html() == "") {
+
+        //  if (count % 2 == 1) {
+        $(this).html("X");
+        $(this).attr("check","X");
+        value = $(this).text();
+
+        checkRow();
+        checkCol();
+        checkDia();
+
+        // } else {
+        //     $(this).html("O")
+        //     value = $(this).text();
+        //     checkRow();
+
+        // }
+        //    }
+
+        
 
     })
+    function checkRow() {
+        for (let j = 1; j < 20; j++) {
+            if ($("[i='" + currentI + "'][j='" + j + "']").attr("check")=="X" &&
+            $("[i='" + currentI + "'][j='" + (j+1) + "']").attr("check")=="X") {
+            // if ($("[i='" + currentI + "'][j='" + j + "']").attr("check") == value && $("[i='" + currentI + "'][j='" + j + "']").attr("check")==$("[i='" + currentI + "'][j='" + (j+1) + "']").attr("check")) {
+                result++
+                
+                if (result == 4){
+                    console.log("win");
+                }
+            }
+           else if (($("[i='" + currentI + "'][j='" + j + "']").attr("check")!= $("[i='" + currentI + "'][j='" + (j+1) + "']").attr("check"))){
+            result = 0
+           }
+        }
+        
+        // for (var j = currentJ - 4; j <= currentJ; j++) {
+        //     if ($("[i='" + currentI + "'][j='" + j + "']").html() == value) {
+        //         result++
+        //         console.log(result);
+        //     }
+        // }
+        // for (var j = currentJ + 4; j >= currentJ; j--) {
+        //     if ($("[i='" + currentI + "'][j='" + j + "']").html() == value) {
+        //         result1++
+        //         console.log("rs1 = " + result1);
+        //     }
+        // }
+        // if (result < 5) {
+        //     result = 0;
+
+        // } else {
+        //     console.log("win");
+        //     //  result = 0
+        // }
+        // if (result1 < 5) {
+        //     result1 = 0
+        // } else {
+        //     console.log("win");
+        //     //  result = 0
+        // }
+    }
+    function checkCol() {
+        for (let i = 1; i < 20; i++) {
+            if ($("[i='" + i + "'][j='" + currentJ + "']").attr("check")=="X" &&
+             $("[i='" + (i+1) + "'][j='" + currentJ + "']").attr("check")=="X") {
+            // if ($("[i='" + currentI + "'][j='" + j + "']").attr("check") == value && $("[i='" + currentI + "'][j='" + j + "']").attr("check")==$("[i='" + currentI + "'][j='" + (j+1) + "']").attr("check")) {
+                result++
+                
+                if (result == 4){
+                    console.log("win");
+                }
+            }
+           else if (($("[i='" + i + "'][j='" + currentJ + "']").attr("check")!= $("[i='" + (i+1) + "'][j='" + (currentJ) + "']").attr("check"))){
+            result = 0
+           }
+        }
+    }
+    function checkDia(){
+        
+        console.log(res);
+       // for (let i = 1 ;i <= 5; i++) {
+            if ($("[i='" + currentI + "'][j='" + currentJ + "']").attr("check")=="X" &&
+             $("[i='" + (currentI-1) + "'][j='" + (currentJ-1) + "']").attr("check")=="X") {
+            // if ($("[i='" + currentI + "'][j='" + j + "']").attr("check") == value && $("[i='" + currentI + "'][j='" + j + "']").attr("check")==$("[i='" + currentI + "'][j='" + (j+1) + "']").attr("check")) {
+             
+                res = res +1;
+                console.log(res);
+                if (res == 4){
+                    console.log("win");
+                }
+                
+            }
+           else if (($("[i='" + currentI + "'][j='" + currentJ + "']").attr("check")!=
+            $("[i='" + (currentI+1) + "'][j='" + (currentJ-1) + "']").attr("check"))){
+                res = 0
+           }
+        
+   // }
+}
+    
     $("button").click(function () {
         location.reload()
     })
