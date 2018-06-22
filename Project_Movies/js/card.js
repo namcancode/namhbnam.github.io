c932475f39b91656eda0ec2c75eaeb48b250f19c
NamBoss <nguyennamit.94@gmail.com>
2018-06-22 17:25:15 +0700

    project: update detail.html

--- Project_Movies/js/card.js
+++ Project_Movies/js/card.js
@@ -930,158 +930,155 @@ function movies (e) {
     window.open("detail.html");
 }
 function showMovies (arguments) {
-    // body
     const dataStorage = JSON.parse(sessionStorage.data)
         contentMovies.innerHTML +=
-          `
-          <div class="movies__media">
-          <div class="media__icon">
-          </div>
-          <div class="media__video">
-              <!-- <iframe src="https://api.banhtv.com/getfb/play.php?q=U2FsdGVkX1%2FB7EsHPwitOJjwTB55BToq4TTkfsRQk%2Bq%2BgoHeKyCMLv9AYtTm6gX6LDANbeTMVHfCWTGqMyVv1viJ3%2FH61MoblskBjMP59DzslI73H8bO3HxGTktO%2FQ2N"
+          `<div class="movies__media">
+    <div class="media__icon">
+    </div>
+    <div class="media__video">
+        <!-- <iframe src="https://api.banhtv.com/getfb/play.php?q=U2FsdGVkX1%2FB7EsHPwitOJjwTB55BToq4TTkfsRQk%2Bq%2BgoHeKyCMLv9AYtTm6gX6LDANbeTMVHfCWTGqMyVv1viJ3%2FH61MoblskBjMP59DzslI73H8bO3HxGTktO%2FQ2N"
                   frameborder="0"></iframe> -->
-              <video width="100%" height="100%" controls>
-                  <source src="https://api.banhtv.com/getfb/play.php?q=U2FsdGVkX1%2FB7EsHPwitOJjwTB55BToq4TTkfsRQk%2Bq%2BgoHeKyCMLv9AYtTm6gX6LDANbeTMVHfCWTGqMyVv1viJ3%2FH61MoblskBjMP59DzslI73H8bO3HxGTktO%2FQ2N"
-                      type="video/mp4">
-              </video>
-          </div>
-      </div>
-      <div class="movies__title">
-          <div class="title__text">
-              <div class="text__top">
-                  <h2>
-                     ${dataStorage.name}
-                  </h2>
-              </div>
-              <div class="text__bot">
-                  <button class="bot__report">
-                      <i class="fas fa-exclamation-triangle"></i>
-                      <p>Báo lỗi</p>
-                  </button>
-                  <button class="bot__block-ads">
-                      <i class="fas fa-power-off"></i>
-                      <p>Tắt quảng cáo</p>
-                  </button>
-                  <div class="bot__add-list">
-                      <i class="fas fa-plus-circle"></i>
-                      <p>Thêm vào danh sách</p>
-                  </div>
-              </div>
-          </div>
-          <div class="title__icon">
+        <video width="100%" height="100%" controls>
+            <source src="https://api.banhtv.com/getfb/play.php?q=U2FsdGVkX1%2FB7EsHPwitOJjwTB55BToq4TTkfsRQk%2Bq%2BgoHeKyCMLv9AYtTm6gX6LDANbeTMVHfCWTGqMyVv1viJ3%2FH61MoblskBjMP59DzslI73H8bO3HxGTktO%2FQ2N"
+                type="video/mp4">
+        </video>
+    </div>
+</div>
+<div class="movies__title">
+    <div class="title__text">
+        <div class="text__top">
+            <h2>
+                ${dataStorage.name}
+            </h2>
+        </div>
+        <div class="text__bot">
+            <button class="bot__report">
+                <i class="fas fa-exclamation-triangle"></i>
+                <p>Báo lỗi</p>
+            </button>
+            <button class="bot__block-ads">
+                <i class="fas fa-power-off"></i>
+                <p>Tắt quảng cáo</p>
+            </button>
+            <div class="bot__add-list">
+                <i class="fas fa-plus-circle"></i>
+                <p>Thêm vào danh sách</p>
+            </div>
+        </div>
+    </div>
+    <div class="title__icon">
 
-              <svg width="61px" height="58px" viewBox="0 0 61 58" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                  <!-- Hello my name is Nam-->
-                  <desc>Created by Nam.</desc>
-                  <defs></defs>
-                  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                      <g id="list_movies-copy" transform="translate(-1198.000000, -685.000000)">
-                          <g id="detail" transform="translate(165.000000, 111.000000)">
-                              <g id="title" transform="translate(0.000000, 541.000000)">
-                                  <g id="quality" transform="translate(1033.288136, 33.000000)">
-                                      <ellipse id="Oval-2" fill="#606060" cx="30.3107345" cy="29" rx="30.3107345" ry="29"></ellipse>
-                                      <text id="HD-720" font-family="AvenirNext-Medium, Avenir Next" font-size="16" font-weight="400" letter-spacing="-0.529999971">
-                                          <tspan x="19.4433333" y="26" fill="#F6B03D">HD</tspan>
-                                          <tspan x="42.2233334" y="26" font-family="AvenirNext-Regular, Avenir Next" font-weight="normal" fill="#F6B03D"></tspan>
-                                          <tspan x="17.4433333" y="48.2" font-family="AvenirNext-Regular, Avenir Next" font-weight="normal" fill="#F9F9F9">72</tspan>
-                                          <tspan x="34.9433334" y="48.2" font-family="AvenirNext-Regular, Avenir Next" font-weight="normal" fill="#F9F9F9">0</tspan>
-                                      </text>
-                                  </g>
-                              </g>
-                          </g>
-                      </g>
-                  </g>
-              </svg>
-          </div>
-      </div>
-      <div class="movies__info">
-          <div class="info__left">
-              <div class="movies-season">
-                  <h1>Các season</h1>
-                  <button>Season 1</button>
-              </div>
-              <div class="movies-data">
-                  <h1>
-                      Nội dung phim
-                  </h1>
-                  <h2>
-                      Phim tập hợp đội siêu anh hùng nổi tiếng nhất - Iron Man, Hulk, Captain America, Thor, Hawkeye và Black Widow, cùng với người
-                      mới Falcon, đánh dấu sự khác biệt của biệt đội anh hùng lần đầu tiên trong lịch sử phim hoạt
-                      hình. Dẫn đầu là Iron Man, những anh hùng được đào tạo và sống chung với nhau tại trụ sở
-                      mới của họ trong tháp Avengers. Nhân vật phản diện nguy hiểm nhất hành tinh thừa cơ hội khi
-                      Avengers tập hợp - dù họ chặn đứng Tiến sĩ Doom khi chinh phục Asgard, Attuma lái xe quân
-                      đội Atlantis vào thành phố New York hay Dracula giải thoát lũ ma cà rồng, Avengers phải chiến
-                      đấu cùng nhau hoàn thành nhiệm vụ.
-                  </h2>
-              </div>
-              <div class="movies-reviews">
-                  <div class="reviews__left">
-                      <div class="actor-film">
-                          <h1>
-                              Diễn viên
-                          </h1>
-                          <h2>
-                              Adrian Pasdar, Fred Tatasciore, Roger Craig Smith
-                          </h2>
-                      </div>
-                      <div class="kind-film">
-                          <h1>
-                              Thể loại
-                          </h1>
-                          <h2>
-                              Phim Khoa Học Viễn Tưởng, Phim Hoạt Hình, Phim Hành Động Phiêu Lưu
-                          </h2>
+        <svg width="61px" height="58px" viewBox="0 0 61 58" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <!-- Hello my name is Nam-->
+            <desc>Created by Nam.</desc>
+            <defs></defs>
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="list_movies-copy" transform="translate(-1198.000000, -685.000000)">
+                    <g id="detail" transform="translate(165.000000, 111.000000)">
+                        <g id="title" transform="translate(0.000000, 541.000000)">
+                            <g id="quality" transform="translate(1033.288136, 33.000000)">
+                                <ellipse id="Oval-2" fill="#606060" cx="30.3107345" cy="29" rx="30.3107345" ry="29"></ellipse>
+                                <text id="HD-720" font-family="AvenirNext-Medium, Avenir Next" font-size="16" font-weight="400" letter-spacing="-0.529999971">
+                                    <tspan x="19.4433333" y="26" fill="#F6B03D">HD</tspan>
+                                    <tspan x="42.2233334" y="26" font-family="AvenirNext-Regular, Avenir Next" font-weight="normal" fill="#F6B03D"></tspan>
+                                    <tspan x="17.4433333" y="48.2" font-family="AvenirNext-Regular, Avenir Next" font-weight="normal" fill="#F9F9F9">72</tspan>
+                                    <tspan x="34.9433334" y="48.2" font-family="AvenirNext-Regular, Avenir Next" font-weight="normal" fill="#F9F9F9">0</tspan>
+                                </text>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    </div>
+</div>
+<div class="movies__info">
+    <div class="info__left">
+        <div class="movies-season">
+            <h1>Các season</h1>
+            <button>Season 1</button>
+        </div>
+        <div class="movies-data">
+            <h1>
+                Nội dung phim
+            </h1>
+            <h2>
+                Phim tập hợp đội siêu anh hùng nổi tiếng nhất - Iron Man, Hulk, Captain America, Thor, Hawkeye và Black Widow, cùng với người
+                mới Falcon, đánh dấu sự khác biệt của biệt đội anh hùng lần đầu tiên trong lịch sử phim hoạt hình. Dẫn đầu
+                là Iron Man, những anh hùng được đào tạo và sống chung với nhau tại trụ sở mới của họ trong tháp Avengers.
+                Nhân vật phản diện nguy hiểm nhất hành tinh thừa cơ hội khi Avengers tập hợp - dù họ chặn đứng Tiến sĩ Doom
+                khi chinh phục Asgard, Attuma lái xe quân đội Atlantis vào thành phố New York hay Dracula giải thoát lũ ma
+                cà rồng, Avengers phải chiến đấu cùng nhau hoàn thành nhiệm vụ.
+            </h2>
+        </div>
+        <div class="movies-reviews">
+            <div class="reviews__left">
+                <div class="actor-film">
+                    <h1>
+                        Diễn viên
+                    </h1>
+                    <h2>
+                        Adrian Pasdar, Fred Tatasciore, Roger Craig Smith
+                    </h2>
+                </div>
+                <div class="kind-film">
+                    <h1>
+                        Thể loại
+                    </h1>
+                    <h2>
+                        Phim Khoa Học Viễn Tưởng, Phim Hoạt Hình, Phim Hành Động Phiêu Lưu
+                    </h2>
 
-                      </div>
-                      <div class="rate-film">
-                          <h1>
-                              Đánh giá của Cinema
-                          </h1>
-                          <div class="rate__star">
-                              <i class="fas fa-star"></i>
-                              <i class="fas fa-star"></i>
-                              <i class="fas fa-star"></i>
-                              <i class="fas fa-star"></i>
-                              <i class="far fa-star"></i>
-                          </div>
-                      </div>
-                      <div class="tag-film">
-                          <span>Tags:</span>
-                          <p>Avengers</p>
-                      </div>
-                  </div>
-                  <div class="revies__right">
-                      <div class="imdb-film">
-                          <h1>Đánh giá</h1>
-                          <h2>8.10(9344 lượt)</h2>
-                      </div>
-                      <div class="directors-film">
-                          <h1>Đạo diễn</h1>
-                          <h2>Eric Radomskiz</h2>
-                      </div>
-                      <div class="country-film">
-                          <h1>Quốc gia</h1>
-                          <h2>Mỹ</h2>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="info__right">
-              <div class="episode">
-                  <div class="episode__circle"></div>
-                  <div class="epsisode__num">
-                      1
-                  </div>
-              </div>
-              <div class="comment">
-                  <!-- comment facebook -->
-                  <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-numposts="4"
-                      data-width='100%'></div>
-                  <!-- main -->
-              </div>
-          </div>
-      </div>
-          `
+                </div>
+                <div class="rate-film">
+                    <h1>
+                        Đánh giá của Cinema
+                    </h1>
+                    <div class="rate__star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i>
+                    </div>
+                </div>
+                <div class="tag-film">
+                    <span>Tags:</span>
+                    <p>Avengers</p>
+                </div>
+            </div>
+            <div class="revies__right">
+                <div class="imdb-film">
+                    <h1>Đánh giá</h1>
+                    <h2>8.10(9344 lượt)</h2>
+                </div>
+                <div class="directors-film">
+                    <h1>Đạo diễn</h1>
+                    <h2>Eric Radomskiz</h2>
+                </div>
+                <div class="country-film">
+                    <h1>Quốc gia</h1>
+                    <h2>Mỹ</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="info__right">
+        <div class="episode">
+            <h1>Các tập tiếp theo</h1>
+            <div class="episode__icon">
+
+                    <p>1</p>
+
+            </div>
+        </div>
+        <div class="comment">
+            <!-- comment facebook -->
+            <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-numposts="4"
+                data-width='100%'></div>
+            <!-- main -->
+        </div>
+    </div>`
 
 
 }
