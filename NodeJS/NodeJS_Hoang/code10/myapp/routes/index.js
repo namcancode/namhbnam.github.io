cdf14937e2204a8ea3765fc4ddd2a90686e3b6ef
Nam Nguyễn <nguyennamit.94@gmail.com>
2018-07-10 00:03:34 +0700

    update APIs

--- NodeJS/NodeJS_Hoang/code10/myapp/routes/index.js
+++ NodeJS/NodeJS_Hoang/code10/myapp/routes/index.js
@@ -1,15 +1,15 @@
 var express = require("express");
 var router = express.Router();
-import { Sequelize } from "../databases/database";
+import { Sequelize, Op } from "../databases/database";
 import { SUCCESS, FAILED } from "../configs/config";
-import { List } from "../models/List";
+import { Detail } from "../models/Detail";
 /* GET home page. */
 router.get("/", function(req, res, next) {
 	res.render("index", { title: "Express" });
 });
 
 router.get("/listAll", (req, res) => {
-	List.findAll({
+	Detail.findAll({
 		attributes: ["name", "priority", "description", "duedate"]
 	})
 		.then(result => {
@@ -30,7 +30,7 @@ router.get("/listAll", (req, res) => {
 
 router.post("/createUser", (req, res) => {
 	const { name, priority, description, duedate } = req.body;
-	List.create(
+	Detail.create(
 		{
 			name,
 			priority: parseInt(priority),
@@ -56,61 +56,143 @@ router.post("/createUser", (req, res) => {
 			});
 		});
 });
-router.get('/listOfset',(req,res)=>{
-  const {offset, limit} = req.query;
-  List.findAll({
-    offset:offset * limit,
-    limit
-  }).then((result) => {
-    res.json({
-      result:SUCCESS,
-      data:result,
-      description:`Thành công`
-    })
-  }).catch((err) => {
-res.json({
-  result:FAILED,
-  data:"",
-  description:`Lỗi ${JSON.stringify(err)}`
-})
-  });
-})
+router.get("/listOfset", (req, res) => {
+	const { offset, limit } = req.query;
+	Detail.findAll({
+		offset: offset * limit,
+		limit
+	})
+		.then(result => {
+			res.json({
+				result: SUCCESS,
+				data: result,
+				description: `Thành công`
+			});
+		})
+		.catch(err => {
+			res.json({
+				result: FAILED,
+				data: "",
+				description: `Lỗi ${JSON.stringify(err)}`
+			});
+		});
+});
 
+router.get("/listById", (req, res) => {
+	//http://localhost:3000/listById?id=3
+	const { id } = req.query;
+	Detail.findOne({
+		attributes: ["id", "name", "priority", "description", "duedate"],
+		where: {
+			id
+		}
+	})
+		.then(result => {
+			if (result) {
+				res.json({
+					result: SUCCESS,
+					data: result,
+					description: `Đã tìm thấy user với id ${id}`
+				});
+			} else {
+				res.json({
+					result: FAILED,
+					data: "",
+					description: `Không tìm thấy user nào với id ${id}`
+				});
+			}
+		})
+		.catch(err => {
+			res.json({
+				result: FAILED,
+				data: "",
+				description: `có lỗi xảy ra trong quá trình tim kiếm ${JSON.stringify(
+					err
+				)}`
+			});
+		});
+});
 
+// router.get("/:nameLike", (req, res) => {
+// 	// req.query
+// 	const { nameLike } = req.params;
+// 	// console.log(`nameLike = ${nameLike}`);
+// 	// Project.findAll({ offset: 5, limit: 5 }); //skip first 5, get next 5
+// 	Detail.findAll({
+// 		attributes: ["id", "name", "priority", "description", "duedate"],
+// 		where: {
+// 			name: {
+// 				// [Op.like]: `%${nameLike}%`,
+// 				[Op.iLike]: `%${nameLike}%`
+// 			}
+// 		}
+// 	})
+// 		.then(foundList => {
+// 			if (foundList) {
+// 				res.json({
+// 					result: SUCCESS,
+// 					data: foundList,
+// 					description: `query List successfully`
+// 				});
+// 			} else {
+// 				res.json({
+// 					result: FAILED,
+// 					data: "",
+// 					description: `Cannot find list with nameLike=${nameLike}`
+// 				});
+// 			}
+// 		})
+// 		.catch(err => {
+// 			res.json({
+// 				result: FAILED,
+// 				data: "",
+// 				description: `Query List failed. Error = ${JSON.stringify(err)}`
+// 			});
+// 		});
+// });
 
-router.get('/listById',(req, res) => {
-  // req.query
-  const { listId } = req.query;
-  List.findOne({
-    attributes: ["id",
-                "name",
-                "priority",
-                "description",
-                "duedate"],
-    where: {
-      id: listId
-    }
-  }).then(foundList => {
-    if (foundList) {
-      res.json({
-          result: SUCCESS,
-          data: foundList,
-          description: `query List successfully with listId=${listId}`
-        });
-    } else {
-      res.json({
-          result: FAILED,
-          data: "",
-          description: `Cannot find list with listId=${listId}`
-        });
-    }
-  }).catch(err => {
-    res.json({
-        result: FAILED,
-        data: "",
-        description: `Query List failed. Error = ${JSON.stringify(err)}`
-    });
-  });
+router.get("/search/:iLike", (req, res) => {
+  const { iLike } = req.params;
+  // console.log(iLike);
+	Detail.findAll({
+		attributes: ["id", "name", "priority", "description", "duedate"],
+		where: {
+			[Op.or]: [
+				{
+					name: {
+						[Op.iLike]: `%${iLike}%`
+					}
+				},
+				{
+					id: {
+						[Op.eq]: `${isNaN(iLike) == 'true' ? iLike : 0}`
+					}
+				}
+			]
+		}
+	})
+		.then(result => {
+			if (result) {
+				res.json({
+					result: SUCCESS,
+					data: result,
+					description: `Đã tìm thấy user ${name} có id là ${id}`
+				});
+			} else {
+				res.json({
+					result: FAILED,
+					data: "",
+					description: `Không tìm thấy user`
+				});
+			}
+		})
+		.catch(err => {
+			res.json({
+				result: FAILED,
+				data: "",
+				description: `Đã có lỗi xảy ra ${err}`
+			});
+		});
 });
 
 module.exports = router;
