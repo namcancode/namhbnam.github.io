58e07a85f2519c862c3acc4159fc0540429fdc07
Nam Nguyễn <nguyennamit.94@gmail.com>
2018-07-23 21:15:47 +0700

    update socket not work

---
+++ NodeJS/NodeJS_Hoang/socketio/myapp/bin/socket.js
@@ -0,0 +1,20 @@
+const app = require("../app");
+var express = require("express");
+const http = require("http").Server(app);
+export const io = require("socket.io")(http);
+
+io.on("connection", function(socket) {
+    console.log("a user connected" + socket);
+    socket.on("disconnect", function() {
+        console.log("user disconnected");
+    });
+    socket.on("send message", function(data) {
+        console.log("message: " + data);
+        io.emit('new message',{msg:data})
+
+    });
+});
+
+http.listen(4000, function() {
+    console.log("listening on *:4000");
+});
