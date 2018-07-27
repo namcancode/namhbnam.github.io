2b42df0903658d313e8093d665bdd75970187a99
Nam Nguyễn <nguyennamit.94@gmail.com>
2018-07-27 16:44:19 +0700

    update i dont know

---
+++ NodeJS/NodeJS_Hoang/mongodb/configs/config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  db: 'mongodb://admin:admin@url:27017/mongodb',
+  db_dev: 'mongodb://admin:admin@url:27017/mongodb',
+};
---
+++ NodeJS/NodeJS_Hoang/sails/config/session.js
@@ -0,0 +1,48 @@
+/**
+ * Session Configuration
+ * (sails.config.session)
+ *
+ * Use the settings below to configure session integration in your app.
+ * (for additional recommended settings, see `config/env/production.js`)
+ *
+ * For all available options, see:
+ * https://sailsjs.com/config/session
+ */
+
+module.exports.session = {
+  adapter: "connect-mongo",
+  url: "mongodb://admin:admin@localhost:27017/mongodb",
+  collection: "sessions",
+  stringify: true,
+  auto_reconnect: false,
+  mongoOptions: {
+    server: {
+      ssl: false
+    }
+  },
+  /***************************************************************************
+  *                                                                          *
+  * Session secret is automatically generated when your new app is created   *
+  * Replace at your own risk in production-- you will invalidate the cookies *
+  * of your users, forcing them to log in again.                             *
+  *                                                                          *
+  ***************************************************************************/
+  secret: '0742f51f1650f3f9d98d5816e56c8c1c',
+
+
+  /***************************************************************************
+  *                                                                          *
+  * Customize when built-in session support will be skipped.                 *
+  *                                                                          *
+  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+  * session management when responding to simple requests for static assets, *
+  * like images or stylesheets.)                                             *
+  *                                                                          *
+  * https://sailsjs.com/config/session                                       *
+  *                                                                          *
+  ***************************************************************************/
+  // isSessionDisabled: function (req){
+  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
+  // },
+
+};
