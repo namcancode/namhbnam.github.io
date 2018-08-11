62735e400757033f4c261b639ea58cabe529b49f
Nam Nguyễn <nguyennamit.94@gmail.com>
2018-08-11 23:55:44 +0700

    update chess

--- NodeJS/NodeJS_Hoang/Game_Chess/js/javascript.js
+++ NodeJS/NodeJS_Hoang/Game_Chess/js/javascript.js
@@ -7,22 +7,22 @@ const showBoard = async () => {
 						`<div class="square ui-widget-content black" id="${8 *
 							i +
 							j}" data-x ="${i}" data-y ="${j}"data-point = "${i},${j}">
-
+						<span>${i},${j}</span>
 
 						</div>`
 				  )
 				: $(".container").append(
 						`<div class="square white" data-x ="${i}" id="${8 * i +
 							j}" data-y ="${j}"data-point = "${i},${j}">
-
+							<span>${i},${j}</span>
 
 						</div>`
 				  );
 		}
 	}
-	await chess();
+	await showChessPieces();
 };
-const chess = async () => {
+const showChessPieces = async () => {
 	// Black chess
 	$("div.square:nth-child(1)").append(
 		`
@@ -262,11 +262,240 @@ const chess = async () => {
 
 $(async () => {
 	await showBoard();
+	function highLightCastle(currentPoint, currentName) {
+		let check = true;
+		if (
+			currentName == "black-castle-left" ||
+			currentName == "black-castle-right" ||
+			currentName == "white-castle-right" ||
+			currentName == "white-castle-left"
+		) {
+			for (
+				let i = parseInt(currentPoint / 8) * 8;
+				i <= (parseInt(currentPoint / 8) + 1) * 8 - 1;
+				i++
+			) {
+				if ($(`#${i}`)[0].childElementCount == 0) {
+					if (check) {
+						check = !check;
+						$(`#${i}`).css("background", "rgb(164, 164, 164)");
+					} else {
+						check = !check;
+						$(`#${i}`).css("background", "rgb(107, 107, 107)");
+					}
+				} else if ($(`#${i}`)[0].childElementCount == 1) {
+					break;
+				}
+			}
+
+			for (
+				let i = currentPoint;
+				i <= parseInt(currentPoint % 8) + 1 + 55;
+				i = i + 8
+			) {
+				if ($(`#${i}`)[0].childElementCount == 0) {
+					if (check) {
+						check = !check;
+						$(`#${i}`).css("background", "rgb(164, 164, 164)");
+					} else {
+						check = !check;
+						$(`#${i}`).css("background", "rgb(107, 107, 107)");
+					}
+				} else if ($(`#${i}`)[0].childElementCount == 1) {
+					break;
+				}
+			}
+			for (
+				let i = currentPoint;
+				i >= parseInt(currentPoint % 8);
+				i = i - 8
+			) {
+				if ($(`#${i}`)[0].childElementCount == 0) {
+					if (check) {
+						check = !check;
+						$(`#${i}`).css("background", "rgb(164, 164, 164)");
+					} else {
+						check = !check;
+						$(`#${i}`).css("background", "rgb(107, 107, 107)");
+					}
+				} else if ($(`#${i}`)[0].childElementCount == 1) {
+					break;
+				}
+			}
+		}
+	}
+	function highLightKnight(currentPoint, currentName) {
+		if (
+			currentName == "black-knight-right" ||
+			currentName == "black-knight-left" ||
+			currentName == "white-knight-right" ||
+			currentName == "white-knight-left"
+		) {
+			let i = parseInt(currentPoint / 8);
+			let j = currentPoint % 8;
+			let L1 = i != 5 ? 8 * (i - 2) + (j - 1) : null;
+			let L2 = 8 * (i - 2) + (j + 1);
+			let L3 = i != 7 && i != 5 ? 8 * (i - 1) + (j - 2) : null;
+			let L4 = i != 0 && i != 7 ? 8 * (i - 1) + (j + 2) : null;
+			let L5 = i != 0 && i != 7 && i != 5 ? 8 * (i + 1) + (j - 2) : null;
+			let L6 = i != 0 ? 8 * (i + 1) + (j + 2) : null;
+			let L7 = i != 5 ? 8 * (i + 2) + (j - 1) : null;
+			let L8 = 8 * (i + 2) + (j + 1);
+
+			$(`#${L1}`).css("background", "rgb(164, 164, 164)");
+
+			$(`#${L2}`).css("background", "rgb(218, 107, 107)");
+
+			$(`#${L3}`).css("background", "rgb(132, 107, 107)");
+
+			$(`#${L4}`).css("background", "rgb(178, 107, 107)");
+
+			$(`#${L5}`).css("background", "rgb(160, 107, 107)");
+
+			$(`#${L6}`).css("background", "rgb(200, 107, 107)");
+
+			$(`#${L7}`).css("background", "rgb(231, 107, 107)");
+
+			$(`#${L8}`).css("background", "rgb(244, 107, 107)");
+
+			// if (
+			// 	$(`#${L1}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "dragon" ||
+			// 	$(`#${L1}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "phoenix"
+			// ) {
+			// 	$(`#${L1}`).css("background", "");
+			// } else {
+			// 	$(`#${L1}`).css(
+			// 		"background",
+			// 		"rgb(164, 164, 164)"
+			// 	);
+			// }
+			// if (
+			// 	$(`#${L2}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "dragon" ||
+			// 	$(`#${L2}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "phoenix"
+			// ) {
+			// 	$(`#${L2}`).css("background", "");
+			// } else {
+			// 	$(`#${L2}`).css(
+			// 		"background",
+			// 		"rgb(107, 107, 107)"
+			// 	);
+			// }
+			// if (
+			// 	$(`#${L3}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "dragon" ||
+			// 	$(`#${L3}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "phoenix"
+			// ) {
+			// 	$(`#${L3}`).css("background", "");
+			// } else {
+			// 	$(`#${L3}`).css(
+			// 		"background",
+			// 		"rgb(107, 107, 107)"
+			// 	);
+			// }
+			// if (
+			// 	$(`#${L4}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "dragon" ||
+			// 	$(`#${L4}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "phoenix"
+			// ) {
+			// 	$(`#${L4}`).css("background", "");
+			// } else {
+			// 	$(`#${L4}`).css(
+			// 		"background",
+			// 		"rgb(107, 107, 107)"
+			// 	);
+			// }
+			// if (
+			// 	$(`#${L5}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "dragon" ||
+			// 	$(`#${L5}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "phoenix"
+			// ) {
+			// 	$(`#${L5}`).css("background", "");
+			// } else {
+			// 	$(`#${L5}`).css(
+			// 		"background",
+			// 		"rgb(107, 107, 107)"
+			// 	);
+			// }
+			// if (
+			// 	$(`#${L6}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "dragon" ||
+			// 	$(`#${L6}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "phoenix"
+			// ) {
+			// 	$(`#${L6}`).css("background", "");
+			// } else {
+			// 	$(`#${L6}`).css(
+			// 		"background",
+			// 		"rgb(107, 107, 107)"
+			// 	);
+			// }
+			// if (
+			// 	$(`#${L7}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "dragon" ||
+			// 	$(`#${L7}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "phoenix"
+			// ) {
+			// 	$(`#${L7}`).css("background", "");
+			// } else {
+			// 	$(`#${L7}`).css(
+			// 		"background",
+			// 		"rgb(107, 107, 107)"
+			// 	);
+			// }
+			// if (
+			// 	$(`#${L8}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "dragon" ||
+			// 	$(`#${L8}`)
+			// 		.find(".chess--piece")
+			// 		.data("team") == "phoenix"
+			// ) {
+			// 	$(`#${L8}`).css("background", "");
+			// } else {
+			// 	$(`#${L8}`).css(
+			// 		"background",
+			// 		"rgb(107, 107, 107)"
+			// 	);
+			// }
+		}
+	}
+	function getPointKnight(n) {
+		let i = parseInt(n / 8);
+		let j = n % 8;
+		let L1 = 8 * (i - 2) + (j - 1);
+		let L2 = 8 * (i - 2) + (j + 1);
+		let L3 = 8 * (i - 1) + (j - 2);
+		let L4 = 8 * (i - 2) + (j + 2);
+		let L5 = 8 * (i + 1) + (j - 2);
+		let L6 = 8 * (i + 2) + (j + 1);
+		let L7 = 8 * (i + 2) + (j - 1);
+		let L8 = 8 * (i + 2) + (j + 1);
+	}
 	function getPointBishop(n) {
 		let t = 0;
-		let i, j;
-		i = parseInt(n / 8);
-		j = n % 8;
+		let i = parseInt(n / 8);
+		let j = n % 8;
 		t = Math.min(i, j);
 		let a = (i - t) * 8 + j - t;
 		t = Math.min(i, 7 - j);
@@ -281,42 +510,21 @@ $(async () => {
 	$(
 		"#black-castle-left, #black-castle-right,#black-knight-right,#black-knight-left,#black-bishop-left,#black-bishop-right,#black-queen,#black-king,#black-pawn-1,#black-pawn-2,#black-pawn-3,#black-pawn-4,#black-pawn-5,#black-pawn-6,#black-pawn-7,#black-pawn-8,#white-castle-left, #white-castle-right,#white-knight-right,#white-knight-left,#white-bishop-left,#white-bishop-right,#white-queen,#white-king,#white-pawn-1,#white-pawn-2,#white-pawn-3,#white-pawn-4,#white-pawn-5,#white-pawn-6,#white-pawn-7,#white-pawn-8"
 	).draggable({
+		addClasses: false,
 		helper: "clone",
 		start: function(event, ui) {
-			let currentName = $(this).attr("id");
-			let currentPoint = $(this)
+			$(this)
 				.parent()
-				.attr("id");
-			if (
-				currentName == "black-castle-left" ||
-				currentName == "black-castle-right" ||
-				currentName == "white-castle-right" ||
-				currentName == "white-castle-left"
-			) {
-				for (
-					let i = parseInt(currentPoint / 8) * 8;
-					i <= (parseInt(currentPoint / 8) + 1) * 8 - 1;
-					i++
-				) {
-					if ($(`#${i}`)[0].childElementCount == 0) {
-						$(`#${i}`).css("box-shadow", "0 0 3px 3px yellow");
-					}
-					// console.log($(`#${i}`)[0].children[0].dataset.team);
+				.toggleClass("hight--light");
 
-				}
-				for (
-					let i = parseInt(currentPoint % 8) + 1 - 1;
-					i <= parseInt(currentPoint % 8) + 1 + 55;
-					i = i + 8
-				) {
-					if ($(`#${i}`)[0].childElementCount == 0) {
-						$(`#${i}`).css("box-shadow", "0 0 3px 3px yellow");
-					}
-					// if($(`#${i}`)[0].children[0].dataset.team == "dragon"){
-					// 	$(".square").css("box-shadow", "");
-					// }
-				}
-			}
+			let currentName = $(this).attr("id");
+			let currentPoint = Number(
+				$(this)
+					.parent()
+					.attr("id")
+			);
+			highLightCastle(currentPoint, currentName);
+			highLightKnight(currentPoint, currentName);
 		},
 		stop: function(event, ui) {
 			let currentName = $(this).attr("id");
@@ -324,13 +532,14 @@ $(async () => {
 				.parent()
 				.find(".fill")
 				.attr("id");
-			$(".square").css("box-shadow", "");
+			$(".square").css("background", "");
 		}
 	});
 	$(".square").droppable({
 		// scope: "tasks", //thằng này để chặn di chuyển
 		tolerance: "pointer", //chỉ cần mouse chỉ vào ô là move
 		drop: function(event, ui) {
+			$(".square").removeClass("hight--light");
 			const startPoint = $(ui.draggable)
 				.parent()[0]
 				.dataset.point.split(",");
