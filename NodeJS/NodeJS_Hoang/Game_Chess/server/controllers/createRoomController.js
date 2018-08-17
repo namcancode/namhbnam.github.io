83acb825508c5623a6743eee971b9b386683c2d2
Nam Nguyễn <nguyennamit.94@gmail.com>
2018-08-17 17:22:16 +0700

    update logic socket

--- NodeJS/NodeJS_Hoang/Game_Chess/server/controllers/createRoomController.js
+++ NodeJS/NodeJS_Hoang/Game_Chess/server/controllers/createRoomController.js
@@ -1,30 +1,42 @@
-import {io,clients} from '../bin/socket';
+import { io, clients } from "../bin/socket";
 
 io.on("connection", function(socket) {
-    // console.log("a user connected" + socket);
-    socket.on("disconnect", function() {
-        console.log("user disconnected");
-        if(clients){
-            for(let i=0; i<clients.length; i++){
-                if(socket.id == clients[i].socket.id){
-                    clients.splice(i, 1);
-                }
-            }
-        }
-    });
-    socket.on("send message", function(data) {
-        // console.log("message: " + data);
-        io.emit('new message',{msg:data})
-    });
-    socket.on("new user",(name)=>{
-        const user = {socket,name};
-        clients.push(user)
-        clients.forEach(e=>{
-            socket.emit('name user',{name:e.name})
-        })
+	// console.log("a user connected" + socket);
+	socket.on("disconnect", function() {
+		// console.log("user disconnected");
+		if (clients) {
+			for (let i = 0; i < clients.length; i++) {
+				if (socket.id == clients[i].id) {
+					clients.splice(i, 1);
+				}
+			}
+		}
+	});
+	socket.on("send message", function(data) {
+		// console.log("message: " + data);
+		io.emit("new message", { msg: data });
+	});
+	socket.on("new user", name => {
+		const user = { id: socket.id, name };
+		if (clients.length > 0) {
+			clients.forEach(e => {
+				if (e.name == name) {
+					socket.emit("cannot create user", { name: e.name });
+					return;
+				}
+			});
+			clients.push(user);
+			socket.join("room total");
+			io.to("room total").emit("name user", clients);
 
-    })
+		} else {
+			clients.push(user);
+			socket.join("room total");
+			io.to("room total").emit("name user", clients);
+		}
 
-
-
+	});
 });
+// const clientInRoom =
+// io.sockets.adapter.rooms["room total"];
+// const keyIDClient = Object.keys(clientInRoom.sockets);
